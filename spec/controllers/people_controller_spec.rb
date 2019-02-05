2339bd39831eee2a4f036db0008e2c5c7a4f2760
Maloney L <maloneyl@users.noreply.github.com>
2019-02-05 17:57:44 +0000

    add tests for success and failure cases

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -4,13 +4,31 @@ describe PeopleController do
   describe "#create" do
     context "when person is valid" do
       it "redirects to #show" do
-        pending "create this test"
+        person_attributes = {first_name: "First"}
+        person = stub_person(person_attributes, valid: true)
+
+        post :create, {person: person_attributes}
+
+        expect(response).to redirect_to(person)
       end
     end
 
     context "when person is invalid" do
       it "renders the 'new' template" do
-        pending "create this test"
+        person_attributes = {first_name: ""}
+        stub_person(person_attributes, valid: false)
+
+        post :create, {person: person_attributes}
+
+        expect(response).to render_template(:new)
+      end
+    end
+
+    def stub_person(attributes, valid:)
+      Person.new.tap do |person|
+        allow(Person).to receive(:new).with(attributes).
+          and_return(person)
+        allow(person).to receive(:save).and_return(valid)
       end
     end
   end
